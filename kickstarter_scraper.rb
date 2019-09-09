365d26559b6e6485d210d78ca51e7712559b5ef4
Patrick Harrison <jahman@bellatlantic.net>
2019-09-09 00:26:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,8 @@
 # require libraries/modules here
+
 require 'pry'
 require 'nokogiri'
+
  def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
@@ -20,17 +22,17 @@ require 'nokogiri'
   # return the projects hash
   projects
 end
-# def create_project_hash
-#   projects = {}
+def create_project_hash
+  projects = {}
  
-#   kickstarter.css("li.project.grid_4").each do |project|
-#     title = project.css("h2.bbcard_name strong a").text
-#     projects[title.to_sym] = {}
-#   end
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
  
-#   # return the projects hash
-#   projects
-# end
+  # return the projects hash
+  projects
+end
 # def create_project_hash
   
 #   html = File.read('fixtures/kickstarter.html')
@@ -51,3 +53,17 @@ end
 # # write your code here
 # end
 # create_project_hash
+
+require 'nokogiri'
+require "pry"
+
+def create_project_hash
+  
+  html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
+ #binding.pry
+
+# write your code here
+end
+
